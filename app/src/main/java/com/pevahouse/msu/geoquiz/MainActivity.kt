19ca4a8a40f1acfb83747bfefcacfd2b0026503b
Laurel <pevahouselaurel@gmail.com>
2023-09-16 00:59:18 -0500

    Chapter 2- Part1 of videos

--- app/src/main/java/com/pevahouse/msu/geoquiz/MainActivity.kt
+++ app/src/main/java/com/pevahouse/msu/geoquiz/MainActivity.kt
@@ -3,57 +3,56 @@ package com.pevahouse.msu.geoquiz
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
+import com.pevahouse.msu.geoquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityMainBinding
+//    private lateinit var trueButton : Button
+//    private lateinit var falseButton : Button
 
-    private lateinit var trueButton : Button
-    private lateinit var falseButton : Button
+    private val questionBank = listOf(
+        //the curse of the comma, had minor issues due to forgetting the comma between questions
+        Question(R.string.question_australia, answer = true) ,
+        Question (R.string.question_oceans, answer = true),
+        Question(R.string.question_mideast, answer = true),
+        Question(R.string.question_africa, answer = true),
+        Question(R.string.question_americas, answer = true),
+        Question(R.string.question_asia, answer = true)
+
+    )
+    private var currentIndex = 0
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         //setting IDs to correlate with buttons
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
+//        trueButton = findViewById(R.id.true_button)
+//        falseButton = findViewById(R.id.false_button)
 
-        //Creating a listener to wait for a click with some toast
-        /*trueButton.setOnClickListener {view: View ->
-            Toast.makeText(
-            //make reference back to file
-            this,
-            R.string.true_button,
-                //had some issues with the following toast, struggled, googled, struggled had forgotten the comma
-            Toast.LENGTH_SHORT)
-            .show()}
 
-        falseButton.setOnClickListener { view: View ->
-            Toast.makeText(
-                this,
-                R.string.false_button,
-                Toast.LENGTH_SHORT)
-                .show()
-        }*/
 
-        trueButton.setOnClickListener {
+        binding.trueButton.setOnClickListener {
             val snackBar = Snackbar.make(
                 it,
-                //do what I say not what I do (Hard coded no no)
                 "Correct",
                 Snackbar.LENGTH_LONG
             )
             snackBar.show()
         }
-        falseButton.setOnClickListener {
+        binding.falseButton.setOnClickListener {
             val snackBar = Snackbar.make(
                 it,
-                //do what I say not what I do (Hard coded no no)
                 "Incorrect",
                 Snackbar.LENGTH_LONG
             )
@@ -61,5 +60,29 @@ class MainActivity : AppCompatActivity() {
             snackBar.setBackgroundTint(Color.RED)
             snackBar.show()
         }
+
+
+        binding.nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            //used parenthesis instead of brackets, broke it momentarily
+//            val questionTextResId = questionBank[currentIndex].textResId
+//            binding.questionTextview.setText(questionTextResId)
+            updateQuestion()
+        }
+
+
+
+        updateQuestion()
+//        val questionTextResId = questionBank[currentIndex].textResId
+//
+//        //overload resolution ambiguity error
+//        //decided to swap and look at other things after typing out few times and reviewing resources
+//        //Saw Question had error, looked, i had int not Int, fixed EVERYTHING.
+//        binding.questionTextview.setText(questionTextResId)
+  }
+
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+           binding.questionTextview.setText(questionTextResId)
     }
 }
